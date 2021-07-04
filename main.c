8d37b9c2e54a71a734e36a1382ce05d0247fc969
Sara Ahmed <sara.sooo481@gmail.com>
2021-07-04 04:43:41 +0200

    initial commit/Student_1

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct student
+{
+	char name[25];
+	int ID;
+	int birth_day;
+	int birth_month;
+	int birth_year;
+	int score;
+
+};
+
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
